aac517d8fa256be68130b0261b5db438fb9c0e88
laki222 <lakibrankovic01@gmail.com>
2023-12-04 16:40:49 +0100

    Docker front and back

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask 
+#from flask_cors import CORS
+
+app = Flask(__name__)
+#CORS(app)
+
+@app.route('/api', methods=['GET'])
+def index():
+  return {
+    "channel": "The Show",
+    "tutorial": "Laki, Flask and Docker"
+  }
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
